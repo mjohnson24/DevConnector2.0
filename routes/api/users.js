fc5bfcc02249e07edacf6df6a02bc90fd254b1f3
mjohnson24 <mjohnson24@comcast.net>
2020-02-10 09:57:08 -0600

    Initial Commit

---
+++ routes/api/users.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route   GET api/users
+// @desc    Test Route
+// @access  Public
+router.post(
+	'/',
+	[
+		check('name', 'Name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			// See if user exists
+			if (user) {
+				res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+			}
+
+			// Get users gravatar
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm',
+			});
+
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
+
+			// Encrypt password
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+
+			// ANYTHING THAT USES A PROMISE, USE ASYNC AWAIT
+			await user.save();
+
+			// Return jsonwebtoken
+
+			res.send('User Registered');
+		} catch (err) {
+			console.log(err.message);
+			res.send(500).send('Server error');
+		}
+	}
+);
+
+module.exports = router;
