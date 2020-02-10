b1d6d60742189b05de4d3cea65accd539838c73b
mjohnson24 <mjohnson24@comcast.net>
2020-02-10 16:07:32 -0600

    Update db.js file to pass parameters in mongoose

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
-const mongoClient = require('mongo-client');
+const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
 	try {
-		await mongoClient(db);
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
 		console.log('MongoDB Connected...');
 	} catch (err) {
 		console.error(err.message);
